72920514ec355e80dc06c692226d0af1d613665b
DARLENE60 <87423684+DARLENE60@users.noreply.github.com>
2021-08-06 14:50:31 +0100

    created the repository and added index.html n main.js

---
+++ main.js
@@ -0,0 +1,57 @@
+// takes in 2 numbers n produces the mod
+
+    const num1 = window.prompt('enter 1st number');
+    const num2 = window.prompt('enter 2nd number');
+    const ans = num1 % num2;
+    alert(ans);
+
+
+// program for a simple calculator
+
+// const number1 = parseFloat(prompt('Enter first number:'));
+// const number2 = parseFloat(prompt('Enter second number:'));
+// const operator = prompt("choose operator\n a for addition\n b for subtraction\n c for multiplication\n d for division")
+
+// if (operator == 'a') {
+//     answer = number1 + number2;
+// }
+// else if (operator == 'b') {
+//     answer = number1 - number2;
+// }
+// else if (operator == 'c') {
+//     answer = number1 * number2;
+// }
+// else if (operator == 'd') {
+//     answer = number1 / number2;
+// }
+// else {
+//     'enter any of the letters above'
+// }
+// alert(answer)
+
+// program to show sasha after 5 counts
+
+// let i;
+// for (i =0; i <= 20; i++) {
+//     alert(i);
+
+//     if (i >= 5 && i % 5 == 0) {
+//         alert('Sasha');
+//     }
+// }
+
+// program for a basic login
+
+// const name = prompt('Enter user name');
+// const password = prompt('Enter user password');
+
+// if (name == 'Darlene' && password == 'password' ){
+//     alert('you have been logged into the system ');
+// }
+// else {
+//     alert('check user name and password');
+// }
+
+
+
+
